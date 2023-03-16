907f5d836e0a9581ce25143398fb4f9484708e61
Mahmoud Mokhtar <mahmokhtar30@gmail.com>
2023-03-16 08:16:19 +0200

    vertical only sectioned page

--- lib/my_Packages/responsive/sectioned_page.dart
+++ lib/my_Packages/responsive/sectioned_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SectionedPage extends StatelessWidget {
-  SectionedPage({super.key});
-
-  final ScrollController hrznScroll = ScrollController();
+  const SectionedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,84 +15,77 @@ class SectionedPage extends StatelessWidget {
             double scrnHeight = constraints.maxHeight;
             double scrnWidth = constraints.maxWidth;
 
-            return Scrollbar(
-              controller: hrznScroll,
-              child: SingleChildScrollView(
-                controller: hrznScroll,
-                scrollDirection: Axis.horizontal,
-                child: SingleChildScrollView(
-                  child: Container(
-                    color: Colors.black26,
-                    constraints: BoxConstraints(
-                      minHeight: scrnHeight,
-                      minWidth: scrnWidth,
-                    ),
-                    child: Column(
+            return SingleChildScrollView(
+              child: Container(
+                color: Colors.black26,
+                constraints: BoxConstraints(
+                  minHeight: scrnHeight,
+                  minWidth: scrnWidth,
+                ),
+                child: Column(
+                  children: [
+                    Row(
                       children: [
-                        Row(
-                          children: [
-                            Container(
-                              height: scrnHeight * .5,
-                              width: scrnWidth * .5,
-                              color: Colors.amber[200],
-                              constraints: const BoxConstraints(
-                                maxHeight: 400,
-                                maxWidth: 400,
-                                minHeight: 100,
-                                minWidth: 100,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              height: scrnHeight * .5,
-                              width: scrnWidth * .5,
-                              color: Colors.amber[200],
-                              constraints: const BoxConstraints(
-                                maxHeight: 400,
-                                maxWidth: 400,
-                                minHeight: 100,
-                                minWidth: 100,
-                              ),
-                            ),
-                          ],
+                        Container(
+                          height: scrnHeight * .5,
+                          width: scrnWidth * .5,
+                          color: Colors.amber[200],
+                          constraints: const BoxConstraints(
+                            maxHeight: 400,
+                            maxWidth: 400,
+                            minHeight: 100,
+                            minWidth: 100,
+                          ),
                         ),
                         const SizedBox(
-                          height: 10,
+                          width: 10,
                         ),
-                        Row(
-                          children: [
-                            Container(
-                              height: scrnHeight * .5,
-                              width: scrnWidth * .5,
-                              color: Colors.amber[200],
-                              constraints: const BoxConstraints(
-                                maxHeight: 400,
-                                maxWidth: 400,
-                                minHeight: 100,
-                                minWidth: 100,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              height: scrnHeight * .5,
-                              width: scrnWidth * .5,
-                              color: Colors.amber[200],
-                              constraints: const BoxConstraints(
-                                maxHeight: 400,
-                                maxWidth: 400,
-                                minHeight: 100,
-                                minWidth: 100,
-                              ),
-                            ),
-                          ],
+                        Container(
+                          height: scrnHeight * .5,
+                          width: scrnWidth * .5,
+                          color: Colors.amber[200],
+                          constraints: const BoxConstraints(
+                            maxHeight: 400,
+                            maxWidth: 400,
+                            minHeight: 100,
+                            minWidth: 100,
+                          ),
                         ),
                       ],
                     ),
-                  ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: scrnHeight * .5,
+                          width: scrnWidth * .5,
+                          color: Colors.amber[200],
+                          constraints: const BoxConstraints(
+                            maxHeight: 400,
+                            maxWidth: 400,
+                            minHeight: 100,
+                            minWidth: 100,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: scrnHeight * .5,
+                          width: scrnWidth * .5,
+                          color: Colors.amber[200],
+                          constraints: const BoxConstraints(
+                            maxHeight: 400,
+                            maxWidth: 400,
+                            minHeight: 100,
+                            minWidth: 100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             );
